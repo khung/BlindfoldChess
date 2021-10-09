6544b6dc0e74906a5c6b1e79484ed3da29898ed5
Kevin Hung <khung@nullaxiom.com>
2021-10-09 12:05:21 -0400

    Changed engine to run in another thread to prevent blocking the GUI.

--- BlindfoldChess.py
+++ BlindfoldChess.py
@@ -1,11 +1,47 @@
 import sys
+from typing import Callable
 
 from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtQml import QQmlApplicationEngine
-from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot
+from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot, QRunnable, QThreadPool
 import chess
 import chess.engine
 import chess.svg
+
+
+class WorkerSignals(QObject):
+    """
+    Define signals available for worker thread.
+    """
+    result = pyqtSignal(object)
+
+
+class Worker(QRunnable):
+    """
+    Worker thread for doing multi-threading in Qt.
+
+    Public methods
+    --------------
+    run
+    """
+
+    def __init__(self, fn: Callable, *args, **kwargs) -> None:
+        """
+        :param fn: Function to run.
+        :param args: Arguments for the function.
+        :param kwargs: Keyword arguments for the function.
+        """
+        super(Worker, self).__init__()
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+        self.signals = WorkerSignals()
+
+    @pyqtSlot()
+    def run(self) -> None:
+        """Run the specified function and return the result as a Qt signal."""
+        result = self.fn(*self.args, **self.kwargs)
+        self.signals.result.emit(result)
 
 
 class Backend(QObject):
@@ -21,7 +57,8 @@ class Backend(QObject):
 
     Instance variables
     ------------------
-    boardChanged
+    player_side
+    engine_time_limit
     """
 
     # Qt signal to indicate that the board has changed
@@ -36,17 +73,20 @@ class Backend(QObject):
         # Connect signals
         self.engineTurn.connect(self.engine_play)
         self.player_side = chess.WHITE
-        self.board = chess.Board()
+        self._board = chess.Board()
         engine_path = r"C:\Users\Kevin Hung\Documents\stockfish_13_win_x64_bmi2.exe"
-        self.engine = chess.engine.SimpleEngine.popen_uci(engine_path)
+        # Use the synchronous wrapper instead of the coroutines to avoid having to deal with an asyncio event loop in
+        # Qt.
+        self._engine = chess.engine.SimpleEngine.popen_uci(engine_path)
+        self.engine_time_limit = 10.0
+        self._thread_pool = QThreadPool()
 
     @pyqtSlot()
     def engine_play(self) -> None:
         """Have the engine think about and make its move."""
-        # This will hang the GUI as it runs in the same thread, but multithreading this is not trivial so we skip it
-        # for now and just limit the engine to 1 second.
-        result = self.engine.play(self.board, chess.engine.Limit(time=1.0))
-        self._push_engine_move(result.move)
+        worker = Worker(self._get_engine_move)
+        worker.signals.result.connect(self._push_engine_move)
+        self._thread_pool.start(worker)
 
     @pyqtSlot(str)
     def push_player_move(self, move: str) -> None:
@@ -56,16 +96,21 @@ class Backend(QObject):
         :param move: The chess move in Standard Algebraic Notation.
         """
         try:
-            self.board.push_san(move)
+            self._board.push_san(move)
         except ValueError:
             print("not a valid move")
             return
         self.draw_current_board()
         self._update_board_status()
         # Only continue game if it is not terminated
-        if self.board.outcome() is None:
+        if self._board.outcome() is None:
             self.engineTurn.emit()
 
+    def _get_engine_move(self) -> chess.Move:
+        result = self._engine.play(self._board, chess.engine.Limit(time=self.engine_time_limit))
+        return result.move
+
+    @pyqtSlot(object)
     def _push_engine_move(self, move: chess.Move) -> None:
         """
         Play the indicated engine's move and update the UI. No need to check legality of the move as the engine should
@@ -73,33 +118,33 @@ class Backend(QObject):
 
         :param move: The chess move given by chess.engine.PlayResult.move.
         """
-        self.board.push(move)
+        self._board.push(move)
         self.draw_current_board()
         self._update_board_status()
         # Only continue game if it is not terminated
-        if self.board.outcome() is None:
+        if self._board.outcome() is None:
             self.playerTurn.emit()
 
     def draw_current_board(self):
         """Draw the current board."""
-        self.boardChanged.emit(self._create_svg_uri(chess.svg.board(self.board)))
+        self.boardChanged.emit(self._create_svg_uri(chess.svg.board(self._board)))
 
     @staticmethod
     def _create_svg_uri(svg_data: str):
         return "data:image/svg+xml;utf8," + svg_data
 
     def _update_board_status(self) -> None:
-        outcome = self.board.outcome()
+        outcome = self._board.outcome()
         if outcome is not None:
             print("outcome: " + outcome.termination.name)
             self.gameOver.emit(outcome.termination.name)
-        elif self.board.is_check():
+        elif self._board.is_check():
             print("in check")
 
     @pyqtSlot()
     def handle_exit(self) -> None:
         """Clean up by stopping the engine. Connect to QGuiApplication.aboutToQuit signal to handle exits correctly."""
-        self.engine.quit()
+        self._engine.quit()
 
 
 if __name__ == '__main__':
